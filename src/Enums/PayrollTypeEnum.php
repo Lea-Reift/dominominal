380030714add446f1a6166646503f7b2e235a495
Manuel Guzman <manuelguzman.villar@gmail.com>
2025-07-05 13:33:33 -0400

    Add descriptions to PayrollTypeEnum cases
    Implemented the HasDescription interface for PayrollTypeEnum and added the getDescription() method to provide descriptions for each enum case.

--- src/Enums/PayrollTypeEnum.php
+++ src/Enums/PayrollTypeEnum.php
@@ -4,12 +4,21 @@ declare(strict_types=1);
 
 namespace App\Enums;
 
+use Filament\Support\Contracts\HasDescription;
 use Filament\Support\Contracts\HasLabel;
 
-enum PayrollTypeEnum: int implements HasLabel
+enum PayrollTypeEnum: int implements HasLabel, HasDescription
 {
     case MONTHLY = 1;
     case BIWEEKLY = 2;
+
+    public function getDescription(): string
+    {
+        return match ($this) {
+            self::MONTHLY => 'Nómina general del mes',
+            self::BIWEEKLY => 'Nómina de una de las mitades del mes'
+        };
+    }
 
     public function getLabel(): string
     {
