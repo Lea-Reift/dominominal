b83ff8b0a68a0637b3b6d803448a7b1b733145f5
Manuel Guzman <manuelguzman.villar@gmail.com>
2025-07-04 23:49:49 -0400

    Fix typo in SalaryAdjustmentTypeEnum case name
    Corrected the case name from DEDUCTIOn to DEDUCTION in SalaryAdjustmentTypeEnum to ensure consistency and prevent potential errors.

--- src/Enums/SalaryAdjustmentTypeEnum.php
+++ src/Enums/SalaryAdjustmentTypeEnum.php
@@ -9,13 +9,13 @@ use Filament\Support\Contracts\HasLabel;
 enum SalaryAdjustmentTypeEnum: int implements HasLabel
 {
     case INCOME = 1;
-    case DEDUCTIOn = 2;
+    case DEDUCTION = 2;
 
     public function getLabel(): string
     {
         return match ($this) {
             self::INCOME => 'Ingreso',
-            self::DEDUCTIOn => 'Descuento',
+            self::DEDUCTION => 'Descuento',
         };
     }
 }
