60cb6a1174afe9be33c9c03c98f4018196c18be2
Manuel Guzman <manuelguzman.villar@gmail.com>
2025-07-06 02:42:47 -0400

    Create displays for models

---
+++ src/Support/ValueObjects/PayrollDisplay.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Support\ValueObjects;
+
+use App\Modules\Payroll\Models\Payroll;
+use App\Support\ValueObjects\PayrollDisplay\TotalRowDisplay;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
+use App\Enums\SalaryAdjustmentTypeEnum;
+use App\Support\ValueObjects\PayrollDisplay\DetailDisplay;
+
+readonly class PayrollDisplay
+{
+    public string $companyName;
+    public string $dateString;
+    public EloquentCollection $incomes;
+    public EloquentCollection $deductions;
+    public Collection $details;
+    public TotalRowDisplay $totals;
+
+    public function __construct(
+        Payroll $payroll
+    ) {
+        $payroll->load([
+            'company',
+            'details' => [
+                'payroll',
+                'employee',
+                'salary',
+                'salaryAdjustments',
+            ],
+            'salaryAdjustments',
+        ]);
+
+        if ($payroll->type->isMonthly()) {
+            $payroll->period = $payroll->period->endOfMonth();
+        }
+
+        $this->companyName = $payroll->company->name;
+
+        $this->dateString = $payroll->period->translatedFormat('d \d\e F \d\e\l Y');
+
+        [
+            SalaryAdjustmentTypeEnum::INCOME->getKey() => $this->incomes,
+            SalaryAdjustmentTypeEnum::DEDUCTION->getKey() => $this->deductions
+        ] = $payroll->salaryAdjustments
+            ->groupBy('type')
+            ->mapWithKeys(
+                fn (Collection $adjustments, int $type) =>
+                [
+                    SalaryAdjustmentTypeEnum::from($type)->getKey() => $adjustments->keyBy('parser_alias'),
+                ]
+            );
+
+        $this->details = $payroll->details->mapInto(DetailDisplay::class)->toBase();
+
+        $this->totals = new TotalRowDisplay($payroll->salaryAdjustments, $this->details);
+    }
+
+    public function render(): View
+    {
+        return view('exports.payroll.index', [
+            'company_name' => $this->companyName,
+            'date_string' => $this->dateString,
+            'incomes' => $this->incomes,
+            'deductions' => $this->deductions,
+            'details' => $this->details,
+            'totals' => $this->totals,
+        ]);
+    }
+}
---
+++ src/Support/ValueObjects/PayrollDisplay/DetailDisplay.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Support\ValueObjects\PayrollDisplay;
+
+use App\Modules\Payroll\Models\PayrollDetail;
+use Illuminate\Support\Collection;
+use App\Enums\SalaryAdjustmentTypeEnum;
+use App\Support\SalaryAdjustmentParser;
+
+readonly class DetailDisplay
+{
+    public string $name;
+    public string $document_number;
+    public float $rawSalary;
+    public Collection $incomes;
+    public Collection $deductions;
+    public float $incomeTotal;
+    public float $deductionTotal;
+    public float $netSalary;
+
+    public function __construct(
+        PayrollDetail $detail
+    ) {
+        $this->name = $detail->employee->full_name;
+        $this->document_number = $detail->employee->document_number;
+        $this->rawSalary = (float) $detail->salary->amount;
+        $adjustments = $detail->payroll->salaryAdjustments->keyBy('parser_alias');
+
+        $adjustments = SalaryAdjustmentParser::make($detail)
+            ->variablesAsCollection()
+            ->groupBy(preserveKeys: true, groupBy: fn (float $_, string $key) => $adjustments->get($key)?->type?->getKey() ?? 'custom');
+
+        $this->incomes = $adjustments[SalaryAdjustmentTypeEnum::INCOME->getKey()];
+        $this->deductions = $adjustments[SalaryAdjustmentTypeEnum::DEDUCTION->getKey()];
+
+        $this->incomeTotal = $this->rawSalary +  $this->incomes->sum();
+        $this->deductionTotal = $this->deductions->sum();
+        $this->netSalary = $this->incomeTotal - $this->deductionTotal;
+    }
+}
