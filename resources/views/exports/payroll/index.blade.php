5e826c5e93b7e6c07b2565648fa93efd115d1799
Manuel Guzman <manuelguzman.villar@gmail.com>
2025-07-06 02:44:36 -0400

    Add payroll export template

---
+++ resources/views/exports/payroll/index.blade.php
@@ -0,0 +1,63 @@
+@php
+    $columnSpan = 6 + $incomes->count() + $deductions->count();    
+@endphp
+
+<table>
+    <thead>
+        <tr>
+            <th colspan="{{ $columnSpan }}"><b>{{ $company_name }}</b></th>
+        </tr>
+        <tr>
+            <th colspan="{{ $columnSpan }}">Nómina Administrativa</th>
+        </tr>
+        <tr>
+            <th colspan="{{ $columnSpan }}">Al {{ $date_string }}</th>
+        </tr>
+        <tr>
+            <th>Colaboradores</th>
+            <th>Cédula</th>
+            <th>Sueldo Bruto</th>
+            @foreach ($incomes as $income)
+                <th>{{ $income->name }}</th>
+            @endforeach
+            <th>Total Ingresos</th>
+            @foreach ($deductions as $deduction)
+                <th>{{ $deduction->name }}</th>
+            @endforeach
+            <th>Total Descuentos</th>
+            <th>Sueldo Neto</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($details as $detail)
+            <tr>
+                <td>{{ $detail->name }}</td>
+                <td>{{ $detail->document_number }}</td>
+                <td>{{ $detail->rawSalary }}</td>
+                @foreach ($incomes->keys() as $income)
+                    <td>{{ $detail->incomes->get($income, 0) }}</td>
+                @endforeach
+                <td>{{ $detail->incomeTotal }}</td>
+                @foreach ($deductions->keys() as $deduction)
+                    <td>{{ $detail->deductions->get($deduction, 0) }}</td>
+                @endforeach
+                <td>{{ $detail->deductionTotal }}</td>
+                <td>{{ $detail->netSalary }}</td>
+            </tr>
+        @endforeach
+        <tr>
+            <td>TOTAL</td>
+            <td></td>
+            <td>{{ $totals->rawSalary }}</td>
+            @foreach ($incomes->keys() as $income)
+                <td>{{ $totals->incomes->get($deduction, 0) }}</td>
+            @endforeach
+            <td>{{ $totals->incomesTotal }}</td>
+            @foreach ($deductions->keys() as $deduction)
+                <td>{{ $totals->deductions->get($deduction, 0) }}</td>
+            @endforeach
+            <td>{{ $totals->deductionsTotal }}</td>
+            <td>{{ $totals->netSalary }}</td>
+        </tr>
+    </tbody>
+</table>
