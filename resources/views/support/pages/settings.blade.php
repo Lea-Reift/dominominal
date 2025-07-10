6c11fb8daaf965c08868cae02212cad02e838b5d
Manuel Guzman <manuelguzman.villar@gmail.com>
2025-07-10 02:10:58 -0400

    Fix form submit button label

--- resources/views/support/pages/settings.blade.php
+++ resources/views/support/pages/settings.blade.php
@@ -6,7 +6,7 @@
         <form wire:submit.prevent="submitEmailSettings" class="space-y-4">
             {{ $emailForm }}
             <x-filament::button type="submit" wire:target="submitEmailSettings" wire:loading.attr="disabled">
-                <span wire:loading.remove wire:target="submitEmailSettings">Guardar General</span>
+                <span wire:loading.remove wire:target="submitEmailSettings">Guardar</span>
                 <span wire:loading wire:target="submitEmailSettings">Guardando...</span>
             </x-filament::button>
         </form>
