f98fcd95476ff61220cce0ff57c46da567d907cd
Manuel Guzman <manuelguzman.villar@gmail.com>
2025-07-12 02:59:35 -0400

    Change server logic to lib

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,22 +1,67 @@
-use tauri::Manager;
+use std::{
+    net::TcpStream,
+    process::{Command, Stdio},
+    sync::{Arc, Mutex},
+    thread,
+    time::Duration,
+};
+
+
+use tauri::{Manager, WindowEvent};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
+    let process = Arc::new(Mutex::new(None));
 
-      // Maximize window
-      app.get_webview_window("main").unwrap().maximize().unwrap();
-      
-      Ok(())
-    })
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    if TcpStream::connect("127.0.0.1:8000").is_err() {
+        let child = Command::new("./resources/php/php.exe")
+            .args([
+                "-S",
+                "127.0.0.1:8000",
+                "../vendor/laravel/framework/src/Illuminate/Foundation/resources/server.php"
+            ])
+            .current_dir("./resources/app/public")
+            .stdout(Stdio::null())
+            .stderr(Stdio::null())
+            .spawn()
+            .expect("Fallo al iniciar el servidor de PHP");
+
+        let mut lock = process.lock().unwrap();
+        *lock = Some(child.id());
+
+        while TcpStream::connect("127.0.0.1:8000").is_err() {
+            thread::sleep(Duration::from_millis(300));
+        }
+    } 
+
+    let cloned_process = process.clone();
+
+    tauri::Builder::default()
+        .setup(|app| {
+            if cfg!(debug_assertions) {
+                app.handle().plugin(
+                    tauri_plugin_log::Builder::default()
+                        .level(log::LevelFilter::Info)
+                        .build(),
+                )?;
+            }
+
+            app.get_webview_window("main").unwrap().maximize().unwrap();
+
+            Ok(())
+        })
+        .on_window_event(move |_, event| match event {
+            WindowEvent::CloseRequested { .. } => {
+
+                if let Some(pid) = cloned_process.lock().unwrap().as_mut() {
+                    Command::new("taskkill")
+                    .args(["/PID", &pid.to_string(), "/F"])
+                    .status()
+                    .expect("Fallo al ejecutar taskkill");
+                }
+            }
+            _ => {}
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,23 +1,5 @@
-#![windows_subsystem = "windows"]
-
-use std::process::Command;
-use std::net::TcpStream;
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-
-    let port_is_reachable: bool = TcpStream::connect("127.0.0.1:8000").is_ok();
-
-    if !port_is_reachable {
-        Command::new("php")
-            .args([
-                "artisan",
-                "serve",
-                "--port=8000"
-            ])
-            .current_dir("../")
-            .spawn()
-            .expect("failed to start php server");
-    }
-
     app_lib::run();
 }
