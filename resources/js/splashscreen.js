f0a421d3aaada7c3db9b45759291375d0c3c9fa3
Manuel Guzman <manuelguzman.villar@gmail.com>
2025-07-18 18:48:37 -0400

    Use main route as check endpoint

--- resources/js/splashscreen.js
+++ resources/js/splashscreen.js
@@ -2,7 +2,7 @@ import { invoke } from "@tauri-apps/api/core";
 
 window.addEventListener('DOMContentLoaded', function () {
     let interval = setInterval(async () => {
-        await fetch("http://localhost:8000/up")
+        await fetch("http://localhost:8000/")
             .then((response) => {
                 if (response.ok) {
                     clearInterval(interval)
