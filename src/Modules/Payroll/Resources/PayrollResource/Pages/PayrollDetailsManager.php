1e3e118ca7df3efcfe50f4f89b29913d012d3637
Manuel Guzman <manuelguzman.villar@gmail.com>
2025-07-27 14:57:09 -0400

    Extract Generate Secondary Payrolls Action to Class

--- src/Modules/Payroll/Resources/PayrollResource/Pages/PayrollDetailsManager.php
+++ src/Modules/Payroll/Resources/PayrollResource/Pages/PayrollDetailsManager.php
@@ -5,11 +5,9 @@ declare(strict_types=1);
 namespace App\Modules\Payroll\Resources\PayrollResource\Pages;
 
 use App\Concerns\HasEmployeeForm;
-use App\Enums\SalaryTypeEnum;
 use App\Modules\Company\Models\Employee;
 use App\Modules\Company\Resources\CompanyResource;
 use App\Modules\Company\Resources\CompanyResource\Pages\ViewCompany;
-use App\Modules\Payroll\Exceptions\DuplicatedPayrollException;
 use App\Modules\Payroll\Models\Payroll;
 use App\Modules\Payroll\Models\PayrollDetail;
 use App\Modules\Payroll\Models\SalaryAdjustment;
@@ -32,7 +30,6 @@ use Filament\Tables\Columns\Summarizers\Summarizer;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Query\Builder;
-use Illuminate\Support\Arr;
 use Illuminate\Support\Number;
 use Filament\Tables\Enums\ActionsPosition;
 use Illuminate\Support\Facades\Mail;
@@ -41,13 +38,11 @@ use App\Modules\Payroll\Actions\HeaderActions\EditPayrollAction;
 use Filament\Actions\ActionGroup;
 use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Support\Facades\DB;
-use Filament\Support\Enums\Alignment;
-use Filament\Support\Exceptions\Halt;
 use Filament\Forms\Components\Tabs;
 use Filament\Forms\Components\Component;
 use Filament\Forms\Components\Tabs\Tab;
 use Illuminate\Support\Str;
+use App\Modules\Payroll\Actions\TableActions\GenerateSecondaryPayrollsAction;
 
 /**
  * @property Payroll $record
@@ -160,18 +155,11 @@ class PayrollDetailsManager extends ManageRelatedRecords
 
     public function table(Table $table): Table
     {
-        $disableSecondaryPayrolls =  Payroll::query()
-            ->whereIn(
-                'period',
-                Arr::mapWithKeys([14, 28], fn (int $day) => [$day => $this->record->period->setDay($day)->toDateString()])
-            )
-            ->exists();
-
         $table
             ->paginated(false)
             ->recordTitleAttribute('employee_id')
             ->headerActions([
-                $this->secondaryPayrollsTableAction($disableSecondaryPayrolls),
+                GenerateSecondaryPayrollsAction::make($this->record),
                 $this->addEmployeesTableAction(),
             ])
             ->actionsPosition(ActionsPosition::BeforeColumns)
@@ -271,127 +259,6 @@ class PayrollDetailsManager extends ManageRelatedRecords
         }
 
         $this->activeFormTab = $currentTab;
-    }
-
-    public function generateSecondaryPayroll(int $day): void
-    {
-        $period = $this->record->period->clone()->setDay($day);
-
-        throw_if(
-            condition: Payroll::query()->where('period', $period)->exists(),
-            exception: DuplicatedPayrollException::make($period)
-        );
-
-        /** @var Payroll $payroll */
-        $payroll = tap($this->record->replicate()
-            ->unsetRelations()
-            ->fill([
-                'type' => SalaryTypeEnum::BIWEEKLY,
-                'period' => $period,
-                'monthly_payroll_id' => $this->record->id,
-            ]))
-            ->save();
-
-        // SalaryAdjustments
-        $payroll->salaryAdjustments()->sync($this->record->salaryAdjustments);
-
-        $details = $this->record->details
-            ->when(
-                $period->day === 14,
-                fn (Collection $details) => $details->reject(
-                    fn (PayrollDetail $detail) => $detail->salary->type->isMonthly()
-                )
-            );
-
-        // Details
-        foreach ($details as $detail) {
-            /**
-             * @var PayrollDetail $newDetail
-             * @var PayrollDetail $detail
-             */
-            $newDetail = tap(
-                $detail->replicate()
-                    ->unsetRelations()
-                    ->fill([
-                        'payroll_id' => $payroll->id,
-                    ])
-            )->save();
-
-            $newDetail->salaryAdjustments()->sync($detail->salaryAdjustments);
-        }
-    }
-
-    protected function secondaryPayrollsTableAction(bool $disableSecondaryPayrolls): Action
-    {
-        return Action::make('secondary_payrolls')
-            ->label('Generar nóminas secundarias')
-            ->modalHeading('Generar nóminas al...')
-            ->visible(fn () => $this->record->type->isMonthly())
-            ->size(ActionSize::Small)
-            ->modalIcon('heroicon-s-clipboard-document')
-            ->databaseTransaction()
-            ->form([
-                CheckboxList::make('dates')
-                    ->label('')
-                    ->bulkToggleable()
-                    ->hint(function () use ($disableSecondaryPayrolls) {
-                        $hasEmployeesWithMonthlySalary = $this->record->employees
-                            ->filter(fn (Employee $employee) => $employee->salary->type->isMonthly())
-                            ->isNotEmpty();
-
-                        return match (true) {
-                            $disableSecondaryPayrolls => 'Las nóminas del mes ya fueron generadas',
-                            $hasEmployeesWithMonthlySalary => 'Los empleados con salarios mensuales no aparecerán en nominas de la primera quincena del mes',
-                            default => '',
-                        };
-                    })
-                    ->hintColor('warning')
-                    ->validationMessages([
-                        'required' => 'Debe seleccionar al menos una fecha'
-                    ])
-                    ->required()
-                    ->gridDirection('row')
-                    ->default(
-                        fn () => DB::query()
-                            ->from($this->record->getTable())
-                            ->select('period')
-                            ->whereIn(
-                                'period',
-                                Arr::mapWithKeys([14, 28], fn (int $day) => [$day => $this->record->period->setDay($day)->toDateString()])
-                            )
-                            ->pluck('period')
-                            ->toArray()
-                    )
-                    ->columns(2)
-                    ->dehydrated(!$disableSecondaryPayrolls)
-                    ->disableOptionWhen(fn (string $value) => Payroll::query()->whereDate('period', $this->record->period->setDay(intval($value)))->exists())
-                    ->options(Arr::mapWithKeys([14, 28], fn (int $day) => [$day => $this->record->period->translatedFormat("{$day} \\d\\e F")])),
-            ])
-            ->color('success')
-            ->modalWidth(MaxWidth::Small)
-            ->modalFooterActionsAlignment(Alignment::Center)
-            ->modalSubmitAction($disableSecondaryPayrolls ? false : null)
-            ->action(function (array $data) {
-                foreach ($data['dates'] as $day) {
-                    try {
-                        $this->generateSecondaryPayroll(intval($day));
-                    } catch (DuplicatedPayrollException $e) {
-                        Notification::make()
-                            ->title('Nóminas Secundarias')
-                            ->danger()
-                            ->body($e->getMessage())
-                            ->send();
-
-                        throw new Halt();
-                    }
-                }
-
-                Notification::make()
-                    ->title('Nóminas Secundarias')
-                    ->success()
-                    ->body('Nóminas generadas con éxito')
-                    ->send();
-            });
     }
 
     protected function addEmployeesTableAction(): Action
