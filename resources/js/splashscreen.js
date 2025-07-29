b0cb1dc35a2cbb944bdc26db1dfa1fba57003835
Manuel Guzman <manuelguzman.villar@gmail.com>
2025-07-29 00:17:21 -0400

    Update splashscreen.js

--- resources/js/splashscreen.js
+++ resources/js/splashscreen.js
@@ -4,7 +4,7 @@ window.addEventListener('DOMContentLoaded', async (e) => {
     let serverStarted = false;
     do {
         const response = await fetch("http://localhost:8000/", { redirect: 'manual' }).catch(() => { });
-        serverStarted = ![301, 302, 200].includes(response?.status);
+        serverStarted = [301, 302, 200].includes(response?.status);
 
         if (serverStarted) {
             await new Promise(resolve => setTimeout(resolve, 1000));
