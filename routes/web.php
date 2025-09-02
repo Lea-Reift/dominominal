926ade5059e3fbc8a9350b73549e4707bb3e3ca3
Manuel Guzman <manuelguzman.villar@gmail.com>
2025-09-02 13:57:38 -0400

    Fix pdf export route

--- routes/web.php
+++ routes/web.php
@@ -2,44 +2,20 @@
 
 declare(strict_types=1);
 
+use App\Modules\Company\Models\Company;
 use App\Modules\Company\Resources\Payrolls\Pages\ViewPayroll;
 use App\Modules\Payroll\Models\Payroll;
 use App\Support\Pages\Setup;
 use Illuminate\Support\Facades\Route;
-use Maatwebsite\Excel\Excel;
-use App\Modules\Payroll\Exports\PayrollExport;
-use App\Modules\Payroll\Models\PayrollDetail;
 use Filament\Facades\Filament;
 
 Route::get('/', Setup::class);
 
-Route::prefix('main/companies/3/payrolls/{payroll}/details/export')
+Route::prefix('main/companies/{company}/payrolls/{payroll}/details/export')
     ->name(ViewPayroll::getRouteName(Filament::getDefaultPanel()) . '.export.')
     ->group(function () {
         Route::name('pdf')
-            ->get('pdf', function (Payroll $payroll) {
+            ->get('pdf', function (Company $company, Payroll $payroll) {
                 return $payroll->display->streamPDF();
-            });
-
-        Route::name('excel')
-            ->get('excel', function (Payroll $payroll) {
-                $filenameDate = $payroll->period;
-
-                $filenameDate = match (true) {
-                    $payroll->type->isMonthly() => $filenameDate->format('m-Y'),
-                    default => $filenameDate->toDateString()
-                };
-
-                return (new PayrollExport($payroll->display))
-                    ->download("Nómina Administrativa {$payroll->company->name} {$filenameDate}.xlsx", Excel::XLSX);
-            });
-    });
-
-Route::prefix('main/payrolls/details/{detail}/export')
-    ->name('filament.main.payrolls.details.show.export.')
-    ->group(function () {
-        Route::name('pdf')
-            ->get('pdf', function (PayrollDetail $detail) {
-                return $detail->display->streamPDF();
             });
     });
