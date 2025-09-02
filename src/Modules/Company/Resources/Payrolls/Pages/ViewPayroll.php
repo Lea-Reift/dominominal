9e5385e9bb466c44bef104d35d8b3668122e4e49
Manuel Guzman <manuelguzman.villar@gmail.com>
2025-09-02 13:58:02 -0400

    Refactor export actions

--- src/Modules/Company/Resources/Payrolls/Pages/ViewPayroll.php
+++ src/Modules/Company/Resources/Payrolls/Pages/ViewPayroll.php
@@ -11,6 +11,7 @@ use App\Modules\Payroll\Exports\PayrollExport;
 use App\Modules\Payroll\Models\Payroll;
 use Filament\Actions\Action;
 use Filament\Actions\ActionGroup;
+use Filament\Facades\Filament;
 use Filament\Resources\Pages\ViewRecord;
 use Illuminate\Support\Str;
 use Maatwebsite\Excel\Excel;
@@ -35,26 +36,20 @@ class ViewPayroll extends ViewRecord
                 ->actions([
                     Action::make('excel_export')
                         ->label('Exportar a Excel')
-                        ->action(function () {
-                            /** @var Payroll $payroll */
-                            $payroll = $this->record;
-                            $filenameDate = $payroll->period;
+                        ->action(function (Payroll $record) {
+                            $filenameDate = $record->period;
 
                             $filenameDate = match (true) {
-                                $payroll->type->isMonthly() => $filenameDate->format('m-Y'),
+                                $record->type->isMonthly() => $filenameDate->format('m-Y'),
                                 default => $filenameDate->toDateString()
                             };
 
-                            return (new PayrollExport($payroll->display))
-                                ->download("Nómina Administrativa {$payroll->company->name} {$filenameDate}.xlsx", Excel::XLSX);
+                            return (new PayrollExport($record->display))
+                                ->download("Nómina Administrativa {$record->company->name} {$filenameDate}.xlsx", Excel::XLSX);
                         }),
                     Action::make('pdf_export')
                         ->label('Exportar a PDF')
-                        ->url(function () {
-                            /** @var Payroll $record */
-                            $record = $this->record;
-                            return $this->getUrl(['record' => $record->id, 'company' => $record->company_id]) . '/export/pdf';
-                        })
+                        ->url(fn (Payroll $record) => route($this->getRouteName(Filament::getPanel()) . '.export.pdf', ['payroll' => $record, 'company' => $record->company_id]))
                         ->openUrlInNewTab(),
                 ]),
         ];
