19c86c45c8fd0c541776f2dae20c0dbc7c9f9ba2
Manuel Guzman <manuelguzman.villar@gmail.com>
2025-09-03 21:03:33 -0400

    Remove unused logs

--- resources/js/splashscreen.js
+++ resources/js/splashscreen.js
@@ -4,8 +4,5 @@ window.addEventListener('DOMContentLoaded', async () => {
     const cookies = await window.cookieStore.getAll();
     if (cookies && cookies.length > 0) {
         await invoke('store_session_cookies', { cookies });
-        console.log(`Stored ${cookies.length} cookies:`, cookies);
-    } else {
-        console.log('No cookies found to store');
     }
 });
