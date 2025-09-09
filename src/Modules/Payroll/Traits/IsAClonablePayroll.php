47786c468a94010bcd7369eb545da8928bbda67c
Manuel Guzman <manuelguzman.villar@gmail.com>
2025-09-09 00:20:58 -0400

    Add payroll cloning trait and update PHPStan config

---
+++ src/Modules/Payroll/Traits/IsAClonablePayroll.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Modules\Payroll\Traits;
+
+use App\Modules\Payroll\Models\Payroll;
+use App\Modules\Payroll\Models\PayrollDetail;
+use InvalidArgumentException;
+use App\Modules\Payroll\Models\SalaryAdjustment;
+
+/**
+ * @mixin Payroll
+ */
+trait IsAClonablePayroll
+{
+    public function cloneInto(Payroll $clonedPayroll, bool $checkIfPayrollIsBiweekly = true): Payroll
+    {
+        throw_if(
+            $checkIfPayrollIsBiweekly && $this->monthly_payroll_id !== null,
+            InvalidArgumentException::class,
+            'No se puede replicar una nómina quincenal'
+        );
+
+        $this->loadMissing([
+            'salaryAdjustments',
+            'details',
+            'biweeklyPayrolls',
+        ]);
+
+        $clonedPayroll->salaryAdjustments()->attach($this->salaryAdjustments->modelKeys());
+
+        $this->details
+            ->each(function (PayrollDetail $detail) use ($clonedPayroll) {
+                $clonedDetail = $detail
+                    ->replicate()
+                    ->unsetRelations()
+                    ->fill(['payroll_id' => $clonedPayroll->id]);
+
+                $clonedDetail->save();
+
+                $adjustments = $detail->salaryAdjustments
+                    ->mapWithKeys(fn (SalaryAdjustment $adjustment) => [
+                        $adjustment->id => ['custom_value' => $adjustment->detailSalaryAdjustmentValue?->custom_value]
+                    ]);
+
+                $clonedDetail->salaryAdjustments()->sync($adjustments);
+            });
+
+        if ($this->biweeklyPayrolls->isNotEmpty()) {
+            $this->biweeklyPayrolls->each(function (Payroll $biweeklyPayroll) use ($clonedPayroll) {
+                $clonedBiweeklyPayroll = $biweeklyPayroll
+                    ->replicate()
+                    ->unsetRelations()
+                    ->fill(['monthly_payroll_id' => $clonedPayroll->id]);
+
+                $clonedBiweeklyPayroll->save();
+
+                $biweeklyPayroll->cloneInto($clonedBiweeklyPayroll, checkIfPayrollIsBiweekly: false);
+            });
+        }
+
+        return $clonedPayroll->fresh([
+            'salaryAdjustments',
+            'details',
+            'biweeklyPayrolls',
+        ]);
+    }
+}
